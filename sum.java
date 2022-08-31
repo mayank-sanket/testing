c2d240f1f4c2fc464f10253d82b39f2287facd4a
mayanksanket7 <87161270+mayanksanket7@users.noreply.github.com>
2022-09-01 04:50:30 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 10;
+
+        System.out.println("The sum of a and b is : " + (a+b));
+    }
+    
+}
